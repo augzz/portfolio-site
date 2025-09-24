a38029446d1db2afc29db96fee8744ae46f2fe15
augzz <auggie.moore124@gmail.com>
2025-09-23 19:31:01 -0700

    commit 5

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const projects = [
   {
     id: 1,
     title: "Active Directory Lab",
-    description: "Virtual lab built using VirtualBox, Linux, Windows, and Splunk.",
+    description: "Virtual lab environment built using VirtualBox, includes Windows, Linux, and Splunk.",
     image: "/projects/AD_Lab_diagram.png",
     tags: ["Active Directory", "Windows", "Linux", "Splunk"],
     githubUrl: "https://github.com/augzz/AD-Lab"
